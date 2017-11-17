14c69a739558cd1198fd6e69f8b4058d864ccce7
Unknown <adflowers@gmail.com>
2017-11-17 11:37:50 -0500

    Initial Prototype
    Basic Functionality Outlined

---
+++ Amazing_Code.cpp
@@ -0,0 +1,14 @@
+#include "car.h"
+#include <string>
+#include <vector>
+#include <iostream>
+
+int main(int argc, char *argv[]) {
+	
+	
+	std::cout<<"This class does some really important things"<<std::endl;
+	
+	
+}
+
+
