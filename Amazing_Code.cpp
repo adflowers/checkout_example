29119d16714f8ba9b0bef5954ee801fc82072b63
Unknown <adflowers@gmail.com>
2017-11-17 11:41:44 -0500

    Alpha 01
    Added Additional Functionality

--- Amazing_Code.cpp
+++ Amazing_Code.cpp
@@ -9,6 +9,9 @@ int main(int argc, char *argv[]) {
 	std::cout<<"This class does some really important things"<<std::endl;
 	
 	
+	std::cout<<"Now this line.  This is where the real magic is."<<std::endl;
+	
+
 }
 
 
